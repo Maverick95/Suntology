aa8db6c7bbf5b7af08aaffc1c94ad59374d44f8f
Nick Emmerson <nickemmerson619@hotmail.com>
2022-05-27 17:28:25 +0100

    Beginning to script out large amounts of test data

---
+++ Suntology/Scripts/CreateTestDataset/V1.sql
@@ -0,0 +1,20 @@
+CREATE OR ALTER PROCEDURE [dbo.suntology.CreateTestDataset]
+AS
+BEGIN
+
+	/* Cleardown. */
+
+	DELETE
+	FROM [suntology.internalcomms];
+
+	DELETE
+	FROM [suntology.member];
+
+	DELETE
+	FROM [suntology.caste];
+
+
+	
+
+
+END
